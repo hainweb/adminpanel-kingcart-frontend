1472934ee4a86aa727d92bd6e99150dd8d49ebb0
hainweb <hainofficialweb@gmail.com>
2025-02-08 18:39:26 +0530

    Update Urls.js

--- src/Components/Urls/Urls.js
+++ src/Components/Urls/Urls.js
@@ -1,4 +1,4 @@
 
-const BASE_URL ='https://adminpanel-kingcart-backend-22.onrender.com/admin';
+const BASE_URL ='https://adminpanel-kingcart-backend.onrender.com/admin';
 const IMG_URL ='';
 export {BASE_URL , IMG_URL}
