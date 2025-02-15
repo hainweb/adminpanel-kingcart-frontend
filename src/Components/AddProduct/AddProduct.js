771d1f49e14c12d18b49b85f85be5de62b1c07f2
hainweb <hainofficialweb@gmail.com>
2025-02-15 19:26:36 +0530

    Update AddProduct.js

--- src/Components/AddProduct/AddProduct.js
+++ src/Components/AddProduct/AddProduct.js
@@ -117,14 +117,11 @@ const AddProduct = () => {
             return new Promise((resolve, reject) => {
                 const img = new Image();
                 img.onload = () => {
-                    if (img.width === 600 && img.height === 600) {
-                        resolve({
+                    resolve({
                             file,
                             preview: URL.createObjectURL(file),
                         });
-                    } else {
-                        reject('All additional images must be 600x600 pixels.');
-                    }
+                    
                 };
                 img.onerror = () => reject('Error loading image file.');
                 img.src = URL.createObjectURL(file);
